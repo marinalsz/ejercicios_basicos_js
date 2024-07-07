2644e7f2e6e3b39b7c03d294ae031faf5bb363b8
Marina <marina.lsz522@gmail.com>
2024-07-07 18:33:20 +0200

    Resuelto ejercicio 3

--- ejercicio3.js
+++ ejercicio3.js
@@ -1,1 +1,25 @@
 //Ejercicio 3
+
+//3.1 
+let mul = 10 * 5;
+console.log(mul);
+
+//3.2 
+let div = 10 / 2;
+console.log(div);
+
+//3.3 
+let resto = 15 % 9;
+console.log(resto);
+
+//3.4
+let p = 10;
+let j = 5;
+let o = p + j;
+console.log(o);
+
+//3.5
+let c = 10;
+let m = 5;
+let i = c * m;
+console.log(i);

d4bdb2e8e09ea3c13c60b0231f8488701939230f
Marina <marina.lsz522@gmail.com>
2024-07-07 14:32:19 +0200

    Resuelto ejercicio 1

---
+++ ejercicio1.js
@@ -0,0 +1,22 @@
+//Ejercicio 1
+
+//1.1. Definición superhéroe favorito
+let miSuperheroeFavorito = "Hulk";
+
+//1.2. Definición número
+let numeroFavorito = 50;
+
+//1.3. Definición dimensiones
+let altura = 5;
+
+let longitud = 10;
+
+//1.4. Cálculo suma
+let suma = altura + longitud;
+
+//1.5. Resultados por pantalla
+console.log("Mi superhéroe favorito es:", miSuperheroeFavorito);
+console.log("Mi número favorito es:", numeroFavorito);
+console.log("La altura es:", altura);
+console.log("La longitud es:", longitud);
+console.log("El resultado de sumar la altura y la longitud es:", suma);
